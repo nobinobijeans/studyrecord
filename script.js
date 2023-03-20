c8d59fffe2edd0456dcb8f849c45653d7303703b
nobinobijeans <115062148+nobinobijeans@users.noreply.github.com>
2023-03-20 14:11:22 +0900

    Update script.js

--- script.js
+++ script.js
@@ -1,100 +1,98 @@
-// 日付を取得
-const date = new Date();
-const year = date.getFullYear();
-const month = ("0" + (date.getMonth() + 1)).slice(-2);
-const day = ("0" + date.getDate()).slice(-2);
-const dateString = `${year}-${month}-${day}`;
+// 日ごとの勉強時間を保存する配列
+let studyData = [];
 
-// フォーム送信時の処理
-const form = document.querySelector("form");
-form.addEventListener("submit", function (event) {
-  event.preventDefault();
+// フォームの送信イベントに対する処理
+document.querySelector('form').addEventListener('submit', function(e) {
+  e.preventDefault(); // デフォルトの送信処理を中止
 
-  const content = document.querySelector("#content").value;
-  const time = parseInt(document.querySelector("#time").value);
+  // フォームの値を取得
+  const studyType = document.querySelector('input[name="study-type"]:checked').value;
+  const studyTime = document.querySelector('input[name="study-time"]').value;
+  const studyDate = document.querySelector('input[name="study-date"]').value;
 
-  // ローカルストレージからデータを取得
-  let data = localStorage.getItem(dateString);
-  if (data === null) {
-    data = {};
-  } else {
-    data = JSON.parse(data);
-  }
+  // 日付をキーとしたオブジェクトを作成し、勉強時間を配列に追加
+  const studyObj = {date: studyDate, type: studyType, time: studyTime};
+  studyData.push(studyObj);
 
-  // データを更新
-  if (data[content] === undefined) {
-    data[content] = time;
-  } else {
-    data[content] += time;
-  }
-
-  // ローカルストレージに保存
-  localStorage.setItem(dateString, JSON.stringify(data));
-
-  // グラフを更新
-  updateGraph();
+  // グラフを描画する
+  drawGraph(studyData);
 });
 
-// グラフを表示
-function updateGraph() {
-  // ローカルストレージからデータを取得
-  const data = JSON.parse(localStorage.getItem(dateString));
+// グラフを描画する関数
+function drawGraph(data) {
+  // グラフの幅、高さ、余白
+  const width = 800;
+  const height = 400;
+  const margin = { top: 20, right: 20, bottom: 30, left: 50 };
 
-  // グラフを描画
-  const graph = document.querySelector("#graph");
-  graph.innerHTML = "";
+  // SVG要素を作成
+  const svg = d3.select("svg");
+  svg.selectAll("*").remove(); // 既存の要素を削除
+  svg.attr("width", width).attr("height", height);
 
-  const graphData = [];
-  let total = 0;
-  for (const key in data) {
-    graphData.push({ name: key, value: data[key] });
-    total += data[key];
-  }
+  // グラフの描画範囲を決定
+  const chart = svg.append("g").attr("transform", `translate(${margin.left}, ${margin.top})`);
+  const innerWidth = width - margin.left - margin.right;
+  const innerHeight = height - margin.top - margin.bottom;
 
-  const colors = {
-    video: "#4285f4",
-    text: "#34a853",
-    others: "#fbbc05",
-  };
+  // 日付の範囲を決定
+  const dateExtent = d3.extent(data, d => new Date(d.date)); // 日付の範囲
+  const xScale = d3.scaleTime().domain(dateExtent).range([0, innerWidth]); // 日付をx座標に対応させる
 
-  const margin = { top: 20, right: 20, bottom: 40, left: 60 };
-  const width = graph.offsetWidth - margin.left - margin.right;
-  const height = graph.offsetHeight - margin.top - margin.bottom;
+  // 勉強時間の範囲を決定
+  const timeExtent = [0, d3.max(data, d => +d.time)]; // 勉強時間の範囲
+  const yScale = d3.scaleLinear().domain(timeExtent).range([innerHeight, 0]); // 勉強時間をy座標に対応させる
 
-  const x = d3
-    .scaleBand()
-    .domain(graphData.map((d) => d.name))
-    .range([0, width])
-    .padding(0.2);
+  var barWidth = (chartWidth / data.length) * 0.8;
+  var bar = chart.selectAll("g")
+      .data(data)
+      .enter().append("g")
+      .attr("transform", function(d, i) { return "translate(" + i * barWidth + ",0)"; });
 
-  const y = d3.scaleLinear().domain([0, total]).range([height, 0]);
+  bar.append("rect")
+      .attr("y", function(d) { return chartHeight - y(d.time); })
+      .attr("height", function(d) { return y(d.time); })
+      .attr("width", barWidth - 1)
+      .style("fill", function(d) {
+          switch(d.type) {
+              case "video":
+                  return "red";
+              case "text":
+                  return "blue";
+              case "the others":
+                  return "green";
+          }
+      });
 
-  const svg = d3
-    .select("#graph")
-    .append("svg")
-    .attr("width", width + margin.left + margin.right)
-    .attr("height", height + margin.top + margin.bottom)
-    .append("g")
-    .attr("transform", `translate(${margin.left}, ${margin.top})`);
+  // Y軸を追加する
+  var yAxis = d3.axisLeft(y);
+  chart.append("g")
+      .attr("class", "y axis")
+      .call(yAxis);
 
-  svg
-    .append("g")
-    .attr("transform", `translate(0, ${height})`)
-    .call(d3.axisBottom(x));
+  // X軸を追加する
+  var xAxis = d3.axisBottom(x);
+  chart.append("g")
+      .attr("class", "x axis")
+      .attr("transform", "translate(0," + chartHeight + ")")
+      .call(xAxis);
 
-  svg.append("g").call(d3.axisLeft(y));
+  // グリッド線を追加する
+  chart.append('g')
+      .attr('class', 'grid')
+      .attr('transform', 'translate(0,' + chartHeight + ')')
+      .call(d3.axisBottom()
+          .scale(x)
+          .tickSize(-chartHeight, 0, 0)
+          .tickFormat('')
+      )
 
-  svg
-    .selectAll("rect")
-    .data(graphData)
-    .enter()
-    .append("rect")
-    .attr("x", (d) => x(d.name))
-    .attr("y", (d) => y(d.value))
-    .attr("width", x.bandwidth())
-    .attr("height", (d) => height - y(d.value))
-    .attr("fill", (d) => colors[d.name]);
-}
+  chart.append('g')
+      .attr('class', 'grid')
+      .call(d3.axisLeft()
+          .scale(y)
+          .tickSize(-chartWidth, 0, 0)
+          .tickFormat('')
+      )
+});
 
-// 初期表示
-updateGraph();
